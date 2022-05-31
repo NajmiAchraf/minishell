15b067d05632be7cd76d555cc9681bf2343c4d6b
Achraf Najmi <anajmi@e1r1p10.1337.ma>
2022-05-31 16:24:46 +0100

    anajmi branch -> 2022-05-31 16:24:46

---
+++ 1push_and_merge.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+
+
+def terminal(command):
+	os.system(command)
+
+
+def main():
+	now = datetime.now()
+	current_date = now.strftime("%Y-%m-%d %H:%M:%S")
+
+	# terminal('@ECHO OFF')
+
+	terminal(f'git add .')
+	terminal(f'git commit -m "anajmi branch -> {current_date}"')
+	terminal(f'git push origin anajmi')
+	terminal(f'git checkout master')
+	terminal(f'git merge anajmi')
+
+
+if __name__ == '__main__':
+	main()
